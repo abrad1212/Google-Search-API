633401bcc2d5e583f97ef3298c9cba6cef3974d9
abrad1212 <abrad12122@gmail.com>
2018-06-14 23:42:56 -0500

    Remove unused modules

--- google/google.py
+++ google/google.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
-from .modules import images
-from .modules import currency
-from .modules import calculator
+#from .modules import images
+#from .modules import currency
+#from .modules import calculator
 from .modules import standard_search
 # from modules import shopping_search
 
@@ -15,10 +15,10 @@ __version__ = "1.1.0"
 """Defines the public inteface of the API."""
 
 search = standard_search.search
-search_images = images.search
-convert_currency = currency.convert
-exchange_rate = currency.exchange_rate
-calculate = calculator.calculate
+#search_images = images.search
+#convert_currency = currency.convert
+#exchange_rate = currency.exchange_rate
+#calculate = calculator.calculate
 
 # TODO: This method is not working anymore! There is a new GET
 # link for this kind of search
